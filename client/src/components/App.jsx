c17c6cc91e8715ecaff640aae8202f13f95747a1
Nick Kim <nicholaskim101@yahoo.com>
2021-05-12 17:20:30 -0700

    'Completed React Workshop Grocery List'

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,30 +1,61 @@
-const App = () => (
-  <div>
-    <img src="grocery-bags.png" />
-    <h1>Grocery List</h1>
-    <form>
-      <label> Item
-        <input name="item" value="" />
-      </label>
-      <label> Quantity
-        <input name="quantity" value="" />
-      </label>
-      <button>Add Grocery</button>
-      <ul className="groceries">
-        <li>
-          <span>apples - </span>
-          <span>4</span>
-        </li>
-        <li>
-          <span>bread - </span>
-          <span>2</span>
-        </li>
-        <li>
-          <span>etc...</span>
-        </li>
-      </ul>
-    </form>
-  </div>
-);
+import React from 'react';
+import groceriesData from '../data/groceriesData.js';
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      groceries: groceriesData,
+      item:'',
+      quantity:''
+    },
+    this.handleChange = this.handleChange.bind(this),
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    var newItem ={
+      name: this.state.item,
+      quantity: this.state.quantity
+    }
+    this.setState({
+      groceries: [newItem, ...this.state.groceries],
+      item:'',
+      quantity:''
+    })
+
+  }
+
+  render() {
+    return(
+
+      <div>
+        <img src="grocery-bags.png" />
+        <h1>Grocery List</h1>
+        <form onSubmit= {this.handleSubmit}>
+          <label> Item
+            <input placeholder='Grocery Item' name="item" value={this.state.item} onChange ={this.handleChange}/>
+          </label>
+          <label> Quantity
+            <input placeholder='Quantity' name="quantity" value={this.state.quantity} onChange ={this.handleChange} />
+          </label>
+          <button>Add Grocery</button>
+          <ul className="groceries">
+            {this.state.groceries.map((obj, index) =>
+              <li key= {index}>{obj.name} - {obj.quantity}</li>
+              )}
+          </ul>
+        </form>
+      </div>
+    )
+  }
+};
 
 export default App;
